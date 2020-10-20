63685a9fd015b589af3f112580e97fab3f4881dc
Kyle Trahan <ktrahan2@gmail.com>
2020-10-20 17:41:33 -0400

    prompts formated, adding json

--- src/HUD/Main.js
+++ src/HUD/Main.js
@@ -1,32 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import { Link } from 'react-router-dom'
 import Inventory from './Inventory'
 import PlayerInformation from './PlayerInformation'
 import Prompt from './Prompt'
 
-function Main() {
-    return (
-        <div>
-            <header className="hud-header">
-                <Link to="/newgame">back to new game</Link>
-                <Link to="/">Home</Link>
-            </header>
-            <main className="player-hud">
-                <div className="hud-body">
-                    <section className="player">
-                        <PlayerInformation/>
-                    </section>
-                    <section className="prompt">
-                       <Prompt/>
-                    </section>
-                    <section className="inventory">
-                        <Inventory/>
-                    </section>
-                </div>
-            </main>
-        </div>
-    )
+class Main extends Component {
+
+    state = {
+        promptNumber: 2,
+    }
+
+    setPromptNumber = (promptNumber) => {
+        this.setState({
+            promptNumber: promptNumber
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <header className="hud-header">
+                    <Link to="/newgame">back to new game</Link>
+                    <Link to="/">Home</Link>
+                </header>
+                <main className="player-hud">
+                    <div className="hud-body">
+                        <section className="player">
+                            <PlayerInformation/>
+                        </section>
+                        <section className="prompt">
+                        <Prompt 
+                            promptNumber={this.state.promptNumber}
+                            setPromptNumber={this.setPromptNumber}
+                        />
+                        </section>
+                        <section className="inventory">
+                            <Inventory/>
+                        </section>
+                    </div>
+                </main>
+            </div>
+        )
+    }
 }
 
 function mapStateToProps(state) {
--- src/HUD/Prompt.js
+++ src/HUD/Prompt.js
@@ -1,16 +1,37 @@
 import React from 'react'
-import Prompts from './config.json'
+import PromptsList from './config.json'
 
-export default function Prompt() {
+export default function Prompt({promptNumber, setPromptNumber}) {
 
-    let selectedPrompt = Prompts.prompts.find(prompt => {
-        return prompt.promptNumber === 1
+    let selectedPrompt = PromptsList.prompts.find(prompt => {
+        return prompt.promptNumber == promptNumber
+    })
+
+    let handleClick = (event) => {
+        setPromptNumber(event.target.id)
+    }
+
+    let selectedPromptText = selectedPrompt.promptText.split("\n")
+    console.log(selectedPromptText)
+    selectedPromptText= selectedPromptText.map(text => {
+        return (
+            <div classname="json-text">
+                <p>{text}</p>
+            </div>
+        )
     })
 
     let selectedPromptOptions = selectedPrompt.promptOptions.map(option => {
         return (
-            <div className="prompt-option">
-                <button className="button">{Object.values(option)}</button>
+            <div 
+                key={option.nextPrompt} 
+                className="prompt-option"
+            >
+                <button 
+                    onClick={handleClick}
+                    id={option.nextPrompt} 
+                    className="button">{Object.values(option)[0]}
+                </button>
             </div>
         )
     })
@@ -18,7 +39,7 @@ export default function Prompt() {
     return (
         <>
             <div className='prompt-header'>
-                <h1 className="current-episode">{selectedPrompt.promptTitle}</h1>
+                <h1 className="current-episode">{selectedPrompt.promptEpisode}</h1>
             </div>
             <div className="prompt-text">
                 <p>{selectedPrompt.promptText}</p>
